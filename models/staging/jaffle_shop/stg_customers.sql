18538843f4f874eac04a0ca81b87451889567cb7
Wenxian Fei <wenxian.fei@cherre.com>
2021-05-18 14:50:46 +0000

    first_practice

---
+++ models/staging/jaffle_shop/stg_customers.sql
@@ -0,0 +1,6 @@
+select
+    id as customer_id,
+    first_name,
+    last_name
+
+from dbt-practice-314016.raw.customers
---
+++ models/staging/stripe/stg_payments.sql
@@ -0,0 +1,5 @@
+select
+    order_id,
+    payment_method,
+    amount
+from dbt-practice-314016.raw.payments
